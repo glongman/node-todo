3ed85c85edc91bb5e566f65776e96f840e8f1c3c
Geoffrey Longman <glongman@gmail.com>
2010-10-09 13:37:50 -0400

    refactoring as I look towards getting testing going

--- config/environment.js
+++ config/environment.js
@@ -1,29 +1,62 @@
-var fs = require('fs'),
-    sys = require('sys');
+if (global.TODO == undefined) {
+  
+  var fs = require('fs'),
+      path = require('path'); 
         
-var AppEnvironment = function() {
-    this.config = {};
-    this.init();
-}
-
-AppEnvironment.prototype.init = function() {
-    this.config = require('./app_config.js');
-    sys.print("Started with app_config: \n" + JSON.stringify(this.config, undefined, 4));
+  var AppEnvironment = function() {
     this.root = fs.realpathSync(__dirname + "/..");
-}
+    this.init();
+  }
 
-var env = new AppEnvironment();
-var requires = [
-    '',
-    '/app',
-    '/lib',
-    '/vendor/connect/lib',
-    '/vendor/express/lib',
-    '/vendor/express/support',
-    '/vendor/node-mongodb-native/lib'
-]
-//set the require paths
-for (i in requires)
-    require.paths.unshift(env.root+requires[i]);
-    
-exports = module.exports = env;
+  AppEnvironment.prototype.init = function() {
+    this.setRequirePaths();
+    this.setNodeEnvironment(process.env.NODE_ENV)
+    this.readConfig(function() {
+      require('app/server');
+    });
+  }
+
+  AppEnvironment.prototype.readConfig = function(callback) {
+    var callback = callback;
+    var self = this;
+    path.exists(self.root+'/config/app_config.js', function(exists) {
+      if (exists) {
+        self.config = require('config/app_config.js');
+        self.config.database = self.config.database[process.env.NODE_ENV];
+        console.log("Started with app_config: \n" + JSON.stringify(self.config, undefined, 4));
+        callback();
+      } else {
+        console.log("File config/app_config.js not found: try `cp config/app_config.js.sample config/app_config.js`");
+        process.exit();
+      }
+    });
+  }
+
+  AppEnvironment.prototype.setRequirePaths = function() {
+    var requires = [
+        '',
+        '/app',
+        '/lib',
+        '/vendor/connect/lib',
+        '/vendor/express/lib',
+        '/vendor/express/support',
+        '/vendor/node-mongodb-native/lib'
+    ]
+    //set the require paths
+    for (i in requires)
+        require.paths.unshift(this.root+requires[i]);
+  }
+
+  AppEnvironment.prototype.setNodeEnvironment = function(environment) {
+    if (environment == undefined) {
+      process.env.NODE_ENV = 'development'; 
+    } else {
+      process.env.NODE_ENV = environment;
+    }
+    console.log("environment is "+process.env.NODE_ENV);
+  }
+  
+  // global
+  global.TODO = new AppEnvironment();
+}    
+exports = module.exports = global.TODO;
--- start.js
+++ start.js
@@ -1,13 +1,4 @@
-var fs = require('fs'), 
-    path = require('path');
-    
-path.exists(fs.realpathSync('config/app_config.js'), function(exists) {
-  if (exists) {
-    require(__dirname + '/config/environment');
-    require('app');    
-  } else {
-    sys.log("File config/app_config.js not found: try `cp config/app_config.js.sample config/app_config.js`");
-    process.exit();
-  }
-})
+require('./config/environment')
+
+
 
